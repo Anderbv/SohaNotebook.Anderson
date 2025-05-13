ddd35bf1400724a18b1fc19401767472bd7e2e99
unknown <hazard20199@gmail.com>
2025-05-12 22:50:57 -0500

    Feat(added users controller and userDto)

--- SohaNotebook.Anderson.API/Controllers/UsersController.cs
+++ SohaNotebook.Anderson.API/Controllers/UsersController.cs
@@ -1,6 +1,40 @@
-﻿namespace SohaNotebook.Anderson.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using SohaNotebook.Anderson.DataService.Data;
+using SohaNotebook.Anderson.Entities.DbSet;
+using SohaNotebook.Anderson.Entities.Dtos;
+
+namespace SohaNotebook.Anderson.Controllers
 {
-    public class UsersController
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class UsersController: ControllerBase
     {
+        private AppDbContext _context;
+        public UsersController(AppDbContext context) { _context = context; }
+        // Get
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.Where(x => x.Status == 1).ToList();
+            return Ok(users);
+        }
+        // Post
+        [HttpPost]
+        public IActionResult AddUser(UserDto user)
+        {
+            var _user = new User();
+            _user.FirstName = user.FirstName;
+            _user.LastName = user.LastName;
+            _user.DateOfBirth = Convert.ToDateTime(user.DateOfBirth);
+            _user.Phone = user.Phone;
+            _user.Email = user.Email;
+            _user.Country = user.Country;
+            _user.Status = 1;
+            _context.Users.Add(_user);
+            _context.SaveChanges();
+            return Ok(); 
+        }
+        // Get All
+
     }
 }
